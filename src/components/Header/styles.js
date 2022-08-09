a4397dea78590a07727028c620dd1dadf39b8e27
Ivanrabelo83 <ivanrabelo85@gmail.com>
2022-08-09 13:44:18 -0300

    Projeto Finanças

---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+height: 150px;
+text-align: center;
+background: #008B45;
+
+`;
+
+export const Header = styled.h1`
+
+`;
+export const Title = styled.div`
+padding-top:20px;
+color: #fff;
+
+
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+
+*{
+    margin:0;
+    padding: 0;
+}
+
+body{
+        font-family: 'Popins', sans-serif;
+    background-color: #00EE76;
+
+}
+
+`;
+
+export default Global;
